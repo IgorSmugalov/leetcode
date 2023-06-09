63eda6b54706b507897346335c1f1762520b72a4
IgorSmugalov <smugalovigor@gmail.com>
2023-06-09 20:38:04 +0300

    Time: 69 ms (81.47%), Space: 45 MB (90.32%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
@@ -0,0 +1,24 @@
+
+
+function evalRPN(tokens: string[]): number {
+    const operations = {
+        '+': (a: number, b: number) => a + b,
+        '-': (a: number, b: number) => a - b,
+        '*': (a: number, b: number) => a * b,
+        '/': (a: number, b: number) => a / b | 0,
+    }
+
+    const stack = []
+    let i = 0
+    while (i < tokens.length) {
+        if (Number.isInteger(Number(tokens[i]))) {
+            stack.push(Number(tokens[i]))
+        } else {
+            const a = stack.pop()
+            const b = stack.pop()
+            stack.push(operations[tokens[i]](b, a))
+        }
+        i++
+    }
+    return stack[0]
+};
