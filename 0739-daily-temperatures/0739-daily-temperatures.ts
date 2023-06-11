c024ca42cbc379d3d2451595e455e9f6f89cd741
IgorSmugalov <smugalovigor@gmail.com>
2023-06-11 22:05:34 +0300

    Time: 283 ms (73.48%), Space: 76 MB (17.28%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -1,16 +1,14 @@
+interface ITemperature {
+  temp: number;
+  day: number;
+}
 function dailyTemperatures(temperatures: number[]): number[] {
-  const stack: { temp: number; day: number }[] = [];
-  const result: number[] = [];
+  const stack: ITemperature[] = [];
+  const result: number[] = new Array(temperatures.length);
   for (let day = temperatures.length - 1; day >= 0; day--) {
     const temp = temperatures[day];
-    while (stack.length && temp >= stack[stack.length - 1].temp) {
-      stack.pop();
-    }
-    if (!stack.length) {
-      result[day] = 0;
-    } else {
-      result[day] = stack[stack.length - 1].day - day;
-    }
+    while (stack.length && temp >= stack[stack.length - 1].temp) stack.pop();
+    result[day] = stack.length ? stack[stack.length - 1].day - day : 0;
     stack.push({ temp, day });
   }
   return result;
