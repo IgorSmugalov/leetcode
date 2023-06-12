7cb3f6007f9b71ba0366d5cd92f3fd33f05a17d9
IgorSmugalov <smugalovigor@gmail.com>
2023-06-12 11:58:08 +0300

    Time: 63 ms (66.25%), Space: 44.3 MB (82.24%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.ts
@@ -0,0 +1,18 @@
+function generate(numRows: number): number[][] {
+  const result: number[][] = [];
+
+  while (result.length < numRows) {
+    const prewRow = result[result.length - 1];
+    if (!prewRow) {
+      result.push([1]);
+      continue;
+    }
+    result.push([]);
+    for (let i = 0; i <= prewRow.length; i++) {
+      if (i === 0 || i === prewRow.length) result[result.length - 1][i] = 1;
+      else result[result.length - 1][i] = prewRow[i - 1] + prewRow[i];
+    }
+  }
+
+  return result;
+}
