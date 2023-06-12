e560ffcf4cee5cf31b76b0598b21e4b4d485cf36
IgorSmugalov <smugalovigor@gmail.com>
2023-06-12 11:59:32 +0300

    Time: 80 ms (25.37%), Space: 46.5 MB (31.24%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,14 @@
+function majorityElement(nums: number[]): number {
+    if (nums.length < 2) return nums[0]
+    const hash = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        if (!hash.has(num)) {
+            hash.set(num, 1)
+        } else {
+            const newNumCoumt = hash.get(num) + 1
+            if (newNumCoumt >= nums.length / 2) return num
+            hash.set(num, newNumCoumt)
+        }
+    }
+};
