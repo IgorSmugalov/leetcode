43d94761e86334d128563d8a0ef76adaa8cc3f37
IgorSmugalov <smugalovigor@gmail.com>
2023-06-13 12:04:54 +0300

    Time: 71 ms (21.29%), Space: 42.9 MB (53.01%) - LeetHub

---
+++ 2762-cache-with-time-limit/2762-cache-with-time-limit.ts
@@ -0,0 +1,28 @@
+interface StoredItem {
+  value: number;
+  timeOut: ReturnType<typeof setTimeout>;
+}
+
+class TimeLimitedCache {
+  store: Map<number, StoredItem> = new Map();
+  constructor() {}
+
+  set(key: number, value: number, duration: number): boolean {
+    const existingKey = this.store.get(key);
+    if (existingKey) clearTimeout(existingKey.timeOut);
+    this.store.set(key, {
+      value,
+      timeOut: setTimeout(() => this.store.delete(key), duration),
+    });
+    return existingKey ? true : false;
+  }
+
+  get(key: number): number {
+    const existingKey = this.store.get(key);
+    return existingKey ? existingKey.value : -1;
+  }
+
+  count(): number {
+    return this.store.size;
+  }
+}
