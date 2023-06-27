baf0c7e7b15d6a3658da6bacf0482d8d3e237b1d
IgorSmugalov <smugalovigor@gmail.com>
2023-06-27 15:51:51 +0300

    Time: 150 ms (49.72%), Space: 63.8 MB (92.59%) - LeetHub

---
+++ 2742-group-by/2742-group-by.ts
@@ -0,0 +1,15 @@
+export {};
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function (fn) {
+  const hash: Record<string, any[]> = {};
+  this.forEach((el) => {
+    const id = fn(el);
+    hash[id] ? hash[id].push(el) : (hash[id] = [el]);
+  });
+  return hash;
+};
