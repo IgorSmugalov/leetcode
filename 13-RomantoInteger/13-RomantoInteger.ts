5d5be166cfa0e70cf099641d0251fea79616ac6a
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 18:36:58 +0300

    File updated at 08.11.2025, 18:36:57

--- 13-RomantoInteger/13-RomantoInteger.ts
+++ 13-RomantoInteger/13-RomantoInteger.ts
@@ -1,4 +1,4 @@
-// Last updated: 08.11.2025, 17:55:30
+// Last updated: 08.11.2025, 18:36:57
 function romanToInt(s: string): number {
     const dict = new Map<string, number>([
         ['I', 1],
@@ -10,9 +10,18 @@ function romanToInt(s: string): number {
         ['M', 1000],
     ]);
 
-    const nums = s.split("").map((rom) => dict.get(rom)).reverse() as number[];
-    return nums.reduce((sum, num, index) => {
-        const nextNum = nums[index + 1] ?? 0
-        return num > nextNum ? sum - nextNum : sum + nextNum;
-    }, nums[0]);
+    const nums = s.split("").map((roman) => dict.get(roman)).reverse() as number[];
+    console.log(nums);
+    let sum = 0;
+    let prev = 0
+    for (const num of nums) {
+        if (num >= prev) {
+            sum += num;
+        } else {
+            sum -= num;
+        }
+        prev = num;
+    }
+
+    return sum;
 };
