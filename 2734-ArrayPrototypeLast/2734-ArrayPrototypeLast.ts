9b3e519663afe93386a2c0c2ebfdf32aa8d4db51
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 16:20:02 +0300

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.ts
@@ -0,0 +1,18 @@
+// Last updated: 08.11.2025, 16:20:02
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    if (this.length === 0) return -1
+    return this[this.length - 1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
