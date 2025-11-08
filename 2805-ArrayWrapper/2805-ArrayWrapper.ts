fa0ddded053e785800a95b26021508a748db3c1c
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 16:19:41 +0300

    Create 2805-ArrayWrapper

---
+++ 2805-ArrayWrapper/2805-ArrayWrapper.ts
@@ -0,0 +1,22 @@
+// Last updated: 08.11.2025, 16:19:41
+class ArrayWrapper {
+	constructor(private nums: number[]) {
+        
+    }
+
+	valueOf() {
+        return this.nums.reduce((acc, curr) => acc+curr, 0)
+    }
+
+	toString() {
+        return JSON.stringify(this.nums);
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
