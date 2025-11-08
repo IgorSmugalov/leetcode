f98d77e20ef0e75e8284a0018a20b5db6d8c41e5
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 00:14:28 +0300

    File updated at 09.11.2025, 00:14:28

--- 242-ValidAnagram/242-ValidAnagram.ts
+++ 242-ValidAnagram/242-ValidAnagram.ts
@@ -1,17 +1,27 @@
-// Last updated: 08.11.2025, 16:20:53
+// Last updated: 09.11.2025, 00:14:28
 function isAnagram(s: string, t: string): boolean {
-  const letterHash: Map<string, number> = new Map();
-  if (s.length !== t.length) return false;
-  for (let index = 0; index < s.length; index++) {
-    const sLetter = s[index];
-    const tLetter = t[index];
-    if (sLetter === tLetter) continue;
-    if (letterHash.has(sLetter))
-      letterHash.set(sLetter, letterHash.get(sLetter)! + 1);
-    else letterHash.set(sLetter, 1);
-    if (letterHash.has(tLetter))
-      letterHash.set(tLetter, letterHash.get(tLetter)! - 1);
-    else letterHash.set(tLetter, -1);
-  }
-  return [...letterHash.values()].every((letter) => letter === 0);
+    if (s.length !== t.length) {
+        return false
+    }
+
+    const sMap = parseStr(s)
+    const tMap = parseStr(t)
+
+    for(const [ char, count ] of sMap) {
+        if (tMap.get(char) !== count) {
+            return false;
+        }
+    }
+
+    return true
+};
+
+function parseStr(string: string) {
+    const map = new Map<string, number>()
+
+    for(const s of string) {
+        map.set(s, (map.get(s) ?? 0) + 1)
+    }
+
+    return map
 }
