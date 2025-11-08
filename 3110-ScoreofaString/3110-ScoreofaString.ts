3c263937b4fdb64699eda86e622e11818801ccbe
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 23:58:35 +0300

    File updated at 08.11.2025, 23:58:35

--- 3110-ScoreofaString/3110-ScoreofaString.ts
+++ 3110-ScoreofaString/3110-ScoreofaString.ts
@@ -1,12 +1,10 @@
-// Last updated: 08.11.2025, 23:56:37
+// Last updated: 08.11.2025, 23:58:35
 function scoreOfString(s: string): number {
-    const scores = s.split("").map((char) => char.charCodeAt(0))
-
     let sum = 0
 
-    for (let i = 0; i < scores.length - 1; i++) {
-        const current = scores[i]
-        const next = scores[i + 1]
+    for (let i = 0; i < s.length - 1; i++) {
+        const current = s.charCodeAt(i)
+        const next = s.charCodeAt(i + 1)
 
         sum += Math.abs(current - next)
     }
