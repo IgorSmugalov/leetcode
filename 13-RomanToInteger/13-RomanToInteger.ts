10d62e9c5eb66e84ef741d4e1a6553c07b568f70
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 16:21:21 +0300

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.ts
@@ -0,0 +1,17 @@
+// Last updated: 08.11.2025, 16:21:21
+function romanToInt(s: string): number {
+    const romanHash: Record<string, number> = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    };
+    const nums: number[] = s.split("").map((rom) => romanHash[rom]);
+    return nums.reduce((sum, num, index) => {
+        return num < nums[index + 1] ? sum - num : sum + num;
+    }, 0);
+
+};
