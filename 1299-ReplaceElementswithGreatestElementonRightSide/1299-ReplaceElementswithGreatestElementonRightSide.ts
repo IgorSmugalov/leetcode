1b3e0c753caf119ebd7bc4e7ec4b62ec93dda174
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 00:57:34 +0300

    File updated at 09.11.2025, 00:57:34

--- 1299-ReplaceElementswithGreatestElementonRightSide/1299-ReplaceElementswithGreatestElementonRightSide.ts
+++ 1299-ReplaceElementswithGreatestElementonRightSide/1299-ReplaceElementswithGreatestElementonRightSide.ts
@@ -1,4 +1,4 @@
-// Last updated: 09.11.2025, 00:57:18
+// Last updated: 09.11.2025, 00:57:34
 function replaceElements(arr: number[]): number[] {
     let max = arr[arr.length - 1]
     arr[arr.length - 1] = -1
@@ -7,7 +7,6 @@ function replaceElements(arr: number[]): number[] {
     for(let i = arr.length - 2; i >= 0; i--) {
         const current = arr[i]
         arr[i] = max
-        const prev = arr[i - 1]
         if (current > max) {
             max = current
         }
