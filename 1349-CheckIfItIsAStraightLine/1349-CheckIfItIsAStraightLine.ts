e64a66c9c4d9bfc486c5f3a9ab5a4537f8114bd9
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 16:20:24 +0300

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.ts
@@ -0,0 +1,10 @@
+// Last updated: 08.11.2025, 16:20:24
+function checkStraightLine(coordinates: number[][]): boolean {
+  const initX = coordinates[0][0];
+  const initY = coordinates[0][1];
+  const offsetX = coordinates[1][0] - initX;
+  const offsetY = coordinates[1][1] - initY;
+  return coordinates.every(
+    ([X, Y]) => offsetY * (X - initX) === offsetX * (Y - initY)
+  );
+}
