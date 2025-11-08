154f91efa173f1c354ea8d831f6bb91981a5d17a
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 15:42:30 +0300

    File updated at 08.11.2025, 15:42:30

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,11 +1,7 @@
-// Last updated: 08.11.2025, 15:41:58
+// Last updated: 08.11.2025, 15:42:30
 function twoSum(nums: number[], target: number): number[] {
     for (let i1 = 0; i1 < nums.length; i1++) {
-        
 
-
-
-        
         for (let i2 = i1 + 1; i2 < nums.length; i2++) {
 
             if (nums[i1] + nums[i2] === target) {
