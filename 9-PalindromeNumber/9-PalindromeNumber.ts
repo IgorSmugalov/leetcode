d409b9980611225eb67248d26a0b902ef145af97
Smugalov Igor <smugalovigor@icloud.com>
2025-11-08 16:21:22 +0300

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.ts
@@ -0,0 +1,17 @@
+// Last updated: 08.11.2025, 16:21:22
+function isPalindrome(x: number): boolean {
+    const str = x.toString()
+
+    let leftIdx = 0
+    let rightIdx = str.length - 1
+
+    while (leftIdx <= rightIdx) {
+        if (str[leftIdx] !== str[rightIdx]) {
+            return false
+        }
+        leftIdx ++
+        rightIdx --
+    }
+
+    return true
+};
