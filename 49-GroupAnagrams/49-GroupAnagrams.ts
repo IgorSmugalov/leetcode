b01145836841c770f3153dd65084985b17b61d1e
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 17:06:17 +0300

    File updated at 09.11.2025, 17:06:17

--- 49-GroupAnagrams/49-GroupAnagrams.ts
+++ 49-GroupAnagrams/49-GroupAnagrams.ts
@@ -1,13 +1,15 @@
-// Last updated: 08.11.2025, 16:21:14
+// Last updated: 09.11.2025, 17:06:17
 function groupAnagrams(strs: string[]): string[][] {
-  const hash: Map<string, string[]> = new Map();
-  for (const string of strs) {
-    const sorted = string.split("").sort().join();
-    if (hash.has(sorted)) {
-      hash.get(sorted)!.push(string);
-    } else {
-      hash.set(sorted, [string]);
+    const map = new Map<string, string[]>()
+
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('')
+        if (map.has(sorted)) {
+            map.get(sorted)!.push(str)
+        } else {
+            map.set(sorted, [str])
+        }
     }
-  }
-  return [...hash.values()];
-}
+
+    return Array.from(map.values())
+};
