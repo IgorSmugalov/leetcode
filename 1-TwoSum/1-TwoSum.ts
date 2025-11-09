3619e12ef4f254a6cf6434859e5ed5b17c408831
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 13:05:22 +0300

    File updated at 09.11.2025, 13:05:22

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,17 +1,16 @@
-// Last updated: 08.11.2025, 16:19:12
-function isPalindrome(x: number): boolean {
-    const str = x.toString()
+// Last updated: 09.11.2025, 13:05:22
+function twoSum(nums: number[], target: number): number[] {
+    
+    for(let i1 = 0; i1 < nums.length; i1++) {
+        const first = nums[i1]
+        
+        for(let i2 = i1 + 1; i2 < nums.length; i2++) {
+            const second = nums[i2]
+            if (first + second === target) {
+                return [i1, i2]
+            }
 
-    let leftIdx = 0
-    let rightIdx = str.length - 1
-
-    while (leftIdx <= rightIdx) {
-        if (str[leftIdx] !== str[rightIdx]) {
-            return false
         }
-        leftIdx ++
-        rightIdx --
     }
-
-    return true
+    return []
 };
