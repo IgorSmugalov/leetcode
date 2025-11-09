3d893f6201b633e0b57ed5430ae62b65ffadff69
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 15:03:28 +0300

    File updated at 09.11.2025, 15:03:28

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.ts
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.ts
@@ -1,16 +1,20 @@
-// Last updated: 08.11.2025, 16:21:20
+// Last updated: 09.11.2025, 15:03:28
 function longestCommonPrefix(strs: string[]): string {
-    if (strs.length === 1) return strs[0]
-    let sbsLength = 1
-    while (true){
-        let sbs = strs[0].substring(0, sbsLength)
-        let isTrue = strs.every((string) => string.substring(0, sbsLength) === sbs && string.length >= sbsLength)
-        if (isTrue) {
-            sbsLength++; 
-            continue; }
-        else {
-            break
-        }
-    } 
-    return strs[0].substring(0, sbsLength - 1)
+    if (strs.length === 1) {
+        return strs[0]
+    }
+
+    if (strs.some(str => str === "")) {
+        return ""
+    }
+
+    let prefix = ''
+    let i = 0
+
+    while (strs.every(str => Boolean(str[i]) && str[i] === strs[0][i])) {
+        prefix += strs[0][i]
+        i++
+    }
+
+    return prefix
 };
