e612619e982f3a9c8e14932a1c598ea3d9a9f72e
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 13:14:58 +0300

    File updated at 09.11.2025, 13:14:58

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,4 +1,4 @@
-// Last updated: 09.11.2025, 13:13:07
+// Last updated: 09.11.2025, 13:14:58
 function twoSum(nums: number[], target: number): number[] {
     const hash = new Map<number, number>()
     // key -> num in nums, value -> num index in nums
@@ -6,7 +6,7 @@ function twoSum(nums: number[], target: number): number[] {
         const num = nums[i]
         const diff = target - num
         if (hash.has(diff)) {
-            return [i, hash.get(diff)]
+            return [hash.get(diff), i]
         }
         hash.set(num, i)
     }
