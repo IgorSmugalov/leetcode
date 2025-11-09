99ebcba4127f119d78108f41f084777791c7bb05
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 14:47:09 +0300

    File updated at 09.11.2025, 14:47:09

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.ts
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.ts
@@ -1,19 +1,21 @@
-// Last updated: 09.11.2025, 14:43:58
+// Last updated: 09.11.2025, 14:47:09
 function findMaxConsecutiveOnes(nums: number[]): number {
     let max = 0
     let current = 0
-    let last = 1
+    let prev = 1
     for (let num of nums) {
-        if (num === 1 && last === 0) {
-            current = 0
-        }
+
         if (num === 1) {
             current++
+        } else {
+            current = 0
         }
+
         if (current > max) {
             max = current
         }
-        last = num
+
+        prev = num
     }
     return max
 };
