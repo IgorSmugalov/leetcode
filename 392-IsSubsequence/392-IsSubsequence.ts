bd7a9ba21d9a3441b9d62e812dacebdcbacd3f0b
Smugalov Igor <smugalovigor@icloud.com>
2025-11-09 13:47:34 +0300

    File updated at 09.11.2025, 13:47:34

--- 392-IsSubsequence/392-IsSubsequence.ts
+++ 392-IsSubsequence/392-IsSubsequence.ts
@@ -1,14 +1,14 @@
-// Last updated: 09.11.2025, 13:42:27
+// Last updated: 09.11.2025, 13:47:34
 function isSubsequence(s: string, t: string): boolean {
-    const sLength = s.length;
-    let subsequenceLength = 0;
-    let i = 0
-    while (sLength !== subsequenceLength && i < t.length) {
-        if (s[subsequenceLength] === t[i]) {
-            subsequenceLength++
+    let sIdx = 0;
+    let tIdx = 0;
+
+    while (sIdx < s.length && tIdx < t.length) {
+        if (s[sIdx] === t[tIdx]) {
+            sIdx++
         }
-        i++
+        tIdx++
     }
 
-    return sLength === subsequenceLength
+    return s.length === sIdx
 };
