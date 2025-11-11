78d2506ed2df3312cfa8cedc8b2508a33c489401
Smugalov Igor <smugalovigor@icloud.com>
2025-11-12 00:17:12 +0300

    Create 680-ValidPalindromeII

---
+++ 680-ValidPalindromeII/680-ValidPalindromeII.ts
@@ -0,0 +1,27 @@
+// Last updated: 12.11.2025, 00:17:11
+function validPalindrome(s: string): boolean {
+    let left = 0
+    let right = s.length - 1
+    while(left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+        }
+
+        left++
+        right--
+    }
+    return true
+};
+
+const isPalindrome = (s: string, left: number, right: number): boolean => {
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false
+        }
+
+        left++
+        right--
+    }
+    return true
+}
