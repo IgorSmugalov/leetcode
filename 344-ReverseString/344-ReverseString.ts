c1a672deb10bd34897db2527faaf4381b51adc8a
Smugalov Igor <smugalovigor@icloud.com>
2025-11-11 22:33:22 +0300

    File updated at 11.11.2025, 22:33:22

--- 344-ReverseString/344-ReverseString.ts
+++ 344-ReverseString/344-ReverseString.ts
@@ -1,15 +1,14 @@
-// Last updated: 08.11.2025, 16:20:51
+// Last updated: 11.11.2025, 22:33:22
 /**
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-  let l = 0
-  let r = s.length - 1
-  while (l < r) {
-      let temp = s[l]
-      s[l] = s[r]
-      s[r] = temp
-      l++
-      r--
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
   }
-};
+}
