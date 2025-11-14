52d88b409df1dc5edb445093cad8e5e7d492912c
Smugalov Igor <smugalovigor@icloud.com>
2025-11-14 20:10:56 +0300

    File updated at 14.11.2025, 20:10:55

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.ts
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.ts
@@ -1,12 +1,10 @@
-// Last updated: 14.11.2025, 20:10:36
+// Last updated: 14.11.2025, 20:10:55
 function removeDuplicates(nums: number[]): number {
     let uniqueIdx = 0
     for(let i = 0; i < nums.length; i++) {
         const number = nums[i]
         const lastUnique = nums[uniqueIdx]
-        if (number === lastUnique) {
-            continue
-        } else {
+        if (number !== lastUnique) {
             uniqueIdx++
             nums[uniqueIdx] = number
         }
